b86ee49342a07b898db2c551abdb199b9cd8e079
Glitch (vuehj23nso) <none>
2021-02-28 03:49:45 +0000

    thank you

--- src/services/Api.js
+++ src/services/Api.js
@@ -5,9 +5,5 @@ export default {
     baseURL: `https://bobbfirstvue.bobberson.repl.co/api`,
     responseType: 'json',
     headers: {"Content-Type": "application/json"}
-  }),
-  Discord: axios.create({
-    baseURL: `https://discord.com/api/v8/users/@me`,
-    headers: {"Content-Type": "application/json","Authorization": `Bot ODAwOTUyNjMzMjQxNTAxNjk2.YAZnRA.5nZxML4yliYXyKKAXB_N8uAalpE`}
   })
 };
